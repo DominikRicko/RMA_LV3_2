2eaafab8545e015d64fdc619278a111fb7d81fdf
Dominik Ričko <dominik.ricko@gmail.com>
2021-04-14 19:37:45 +0200

    added recycler view to main activity layout

---
+++ app/src/main/java/hr/dominikricko/rma_lv2/activities/MainActivity.kt
@@ -0,0 +1,24 @@
+package hr.dominikricko.rma_lv2.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.dominikricko.rma_lv2.adapters.PersonAdapter
+import hr.dominikricko.rma_lv2.databinding.ActivityMainBinding
+import hr.dominikricko.rma_lv2.listeners.ShowRandomQuoteOnClick
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var recyclerAdapter : PersonAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        recyclerAdapter = PersonAdapter(ShowRandomQuoteOnClick)
+
+        ActivityMainBinding.inflate(layoutInflater)
+        mainBinding.rvPeople.adapter = recyclerAdapter
+
+        setContentView(mainBinding.root)
+    }
+}
