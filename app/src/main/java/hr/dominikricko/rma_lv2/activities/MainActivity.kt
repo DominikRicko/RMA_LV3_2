66fb3af50b968de7572efb6c3603546173539d1a
Dominik Ričko <dominik.ricko@gmail.com>
2021-04-29 22:03:05 +0200

    first working fragment code

--- app/src/main/java/hr/dominikricko/rma_lv2/activities/MainActivity.kt
+++ app/src/main/java/hr/dominikricko/rma_lv2/activities/MainActivity.kt
@@ -1,37 +1,29 @@
 package hr.dominikricko.rma_lv2.activities
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import hr.dominikricko.rma_lv2.adapters.PersonAdapter
+import hr.dominikricko.rma_lv2.R
 import hr.dominikricko.rma_lv2.data.DefaultPeopleAdded
 import hr.dominikricko.rma_lv2.databinding.ActivityMainBinding
-import hr.dominikricko.rma_lv2.listeners.ShowRandomQuoteOnClick
+import hr.dominikricko.rma_lv2.fragments.DisplayPeopleFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
-    private lateinit var recyclerAdapter: PersonAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        recyclerAdapter = PersonAdapter(ShowRandomQuoteOnClick)
-
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        mainBinding.rvPeople.adapter = recyclerAdapter
-        mainBinding.rvPeople.layoutManager = LinearLayoutManager(this)
 
-        mainBinding.btnAddNewPerson.setOnClickListener { createNewPerson() }
+        if( savedInstanceState == null){
+            DefaultPeopleAdded.init()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fl_fragmentContainer, DisplayPeopleFragment())
+                .commit()
+        }
 
-        DefaultPeopleAdded.init()
         setContentView(mainBinding.root)
-    }
-
-    private fun createNewPerson() {
-        val newPersonIntent = Intent(this, AddPersonFragment::class.java)
-        startActivity(newPersonIntent)
     }
 
 }
--- app/src/main/java/hr/dominikricko/rma_lv2/fragments/DisplayPeopleFragment.kt
+++ app/src/main/java/hr/dominikricko/rma_lv2/fragments/DisplayPeopleFragment.kt
@@ -1,6 +1,5 @@
 package hr.dominikricko.rma_lv2.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,16 +10,6 @@ import hr.dominikricko.rma_lv2.adapters.PersonAdapter
 import hr.dominikricko.rma_lv2.databinding.FragmentDisplayPeopleBinding
 import hr.dominikricko.rma_lv2.listeners.ShowRandomQuoteOnClick
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DisplayPeopleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DisplayPeopleFragment : Fragment() {
 
     private lateinit var binding: FragmentDisplayPeopleBinding
@@ -43,26 +32,6 @@ class DisplayPeopleFragment : Fragment() {
         binding.btnAddNewPerson.setOnClickListener { createNewPerson() }
 
         return binding.root;
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DisplayPeopleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DisplayPeopleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
     private fun createNewPerson() {
